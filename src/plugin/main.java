f2c3ea9de7633b8ca17eb5471636cce0662a163b
Ilya <86732040+Gitsans66@users.noreply.github.com>
2022-05-29 16:14:09 +0300

    Update main.java

--- src/plugin/main.java
+++ src/plugin/main.java
@@ -18,14 +18,20 @@ import java.util.*;
 import java.lang.*;
 
 public class main extends Plugin {
+	public int fx, fy, tx, ty;
+
 	@Override
 	public void registerClientCommands(CommandHandler handler) {
 		handler.<Player>register("chaos", "Started chaos", (args, player) -> {
 			UnitType flare = Vars.content.units().find(b -> b.name.equals("flare"));
 			UnitType toxopid = Vars.content.units().find(b -> b.name.equals("toxopid"));
 			for (int i = 0; i < 10; i++) {
-				flare.spawn(Team.crux, (int)(Math.random()*Vars.world.width()*10, (int)(Math.random()*Vars.world.height())*10));
-				toxopid.spawn(Team.crux, (int)(Math.random()*Vars.world.width())*10, (int)(Math.random()*Vars.world.height()*10));
+				fx = (int)(Math.random()*Vars.world.width()*10);
+				fy = (int)(Math.random()*Vars.world.height()*10);
+				tx = (int)(Math.random()*Vars.world.width()*10);
+				ty = (int)(Math.random()*Vars.world.height()*10);
+				flare.spawn(Team.crux, fx, fy);
+				toxopid.spawn(Team.crux, tx, ty);
 			}
 		});
 	}
