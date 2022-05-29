0c2d904c96a13148038c4dd329f1128e3360774d
Ilya <86732040+Gitsans66@users.noreply.github.com>
2022-05-29 13:14:36 +0300

    Create main.java

---
+++ src/plugin/main.java
@@ -0,0 +1,28 @@
+package plugin;
+
+import arc.util.*;
+import arc.*;
+import arc.graphics.*;
+
+import mindustry.mod.*;
+import mindustry.Vars;
+import mindustry.gen.*;
+import mindustry.game.*;
+import mindustry.net.*;
+import mindustry.game.EventType.*;
+import mindustry.content.*;
+import mindustry.type.*;
+import mindustry.entities.*;
+
+import java.util.*;
+
+public class main extends Plugin {
+	@Override
+	public void registerClientCommands(CommandHandler handler) {
+		handler.<Player>register("chaos", "Started chaos", (args, player)) {
+			UnitType mono = Vars.content.units().find(b -> b.name.equals("mono"));
+			mono.spawn(crux.team(), player.x * 3 - player.x, player.y * 3 - player.y);
+			
+		}
+	}
+}
