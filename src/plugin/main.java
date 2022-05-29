60e0b706d6b66cb124e316bf8ec5b12dd95afb42
Ilya <86732040+Gitsans66@users.noreply.github.com>
2022-05-29 13:18:51 +0300

    Update main.java

--- src/plugin/main.java
+++ src/plugin/main.java
@@ -19,10 +19,10 @@ import java.util.*;
 public class main extends Plugin {
 	@Override
 	public void registerClientCommands(CommandHandler handler) {
-		handler.<Player>register("chaos", "Started chaos", (args, player)) {
+		handler.<Player>register("chaos", "Started chaos", (args, player) -> {
 			UnitType mono = Vars.content.units().find(b -> b.name.equals("mono"));
 			mono.spawn(crux.team(), player.x * 3 - player.x, player.y * 3 - player.y);
-			
+
 		}
-	}
+	});
 }
