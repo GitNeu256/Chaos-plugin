f6f6be92a260b53c8561c9713ba81487c904b31e
Ilya <86732040+Gitsans66@users.noreply.github.com>
2022-05-29 19:27:41 +0300

    Update main.java

--- src/plugin/main.java
+++ src/plugin/main.java
@@ -25,12 +25,34 @@ public class main extends Plugin {
 		handler.<Player>register("chaos", "Started chaos", (args, player) -> {
 			UnitType flare = Vars.content.units().find(b -> b.name.equals("flare"));
 			UnitType toxopid = Vars.content.units().find(b -> b.name.equals("toxopid"));
-			for (int i = 0; i < 10; i++) {
+			UnitType antumbra = Vars.content.units().find(b -> b.name.equals("antumbra"));
+			UnitType crawler = Vars.content.units().find(b -> b.name.equals("crawler"))
+			UnitType zenith = Vars.content.units().find(b -> b.name.equals("zenith"))
+
+			for (int i = 0; i < 200; i++) {
+				cx = (int)(Math.random()*Vars.world.width()*10);
+				cy = (int)(Math.random()*Vars.world.height()*10);
+				crawler.spawn(Team.crux, cx, cy);
+			}
+
+			for (int i = 0; i < 50; i++) {
 				fx = (int)(Math.random()*Vars.world.width()*10);
 				fy = (int)(Math.random()*Vars.world.height()*10);
+				zx = (int)(Math.random()*Vars.world.width()*10);
+				zy = (int)(Math.random()*Vars.world.height()*10);
+				flare.spawn(Team.crux, zx, zy);
+				zenith.spawn(Team.crux, )
+			}
+
+			for (int i = 0; i < 20; i++) {
+				ax = (int)(Math.random()*Vars.world.width()*10);
+				ay = (int)(Math.random()*Vars.world.height()*10);
+				antumbra.spawn(Team.crux, ax, ay);
+			}
+
+			for (int i = 0; i < 10; i++) {
 				tx = (int)(Math.random()*Vars.world.width()*10);
 				ty = (int)(Math.random()*Vars.world.height()*10);
-				flare.spawn(Team.crux, fx, fy);
 				toxopid.spawn(Team.crux, tx, ty);
 			}
 		});
