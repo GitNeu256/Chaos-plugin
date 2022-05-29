d855d37fd91896638da275bbc9dd35d8714fe5ce
Ilya <86732040+Gitsans66@users.noreply.github.com>
2022-05-29 14:24:21 +0300

    Update main.java

--- src/plugin/main.java
+++ src/plugin/main.java
@@ -17,17 +17,13 @@ import mindustry.entities.*;
 import java.util.*;
 
 public class main extends Plugin {
-	public int score;
-
 	@Override
 	public void registerClientCommands(CommandHandler handler) {
 		handler.<Player>register("chaos", "Started chaos", (args, player) -> {
 			UnitType flare = Vars.content.units().find(b -> b.name.equals("flare"));
-			if (score < 10){
-				flare.spawn(Team.crux, player.x * 2 - player.x, player.y * 2 - player.y);
-				score = score + 1;
+			for (int i = 0; i < 10; i++) {
+				flare.spawn(Team.crux, player.x * 2, player.y * 2);
 			}
-			score = 0;
 		});
 	}
 }
