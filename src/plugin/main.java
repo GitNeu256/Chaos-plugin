21482f597098919dfd0e706161dc96c59c426125
Ilya <86732040+Gitsans66@users.noreply.github.com>
2022-05-29 14:48:50 +0300

    Update main.java

--- src/plugin/main.java
+++ src/plugin/main.java
@@ -15,14 +15,18 @@ import mindustry.type.*;
 import mindustry.entities.*;
 
 import java.util.*;
+import java.lang.*;
 
 public class main extends Plugin {
 	@Override
 	public void registerClientCommands(CommandHandler handler) {
 		handler.<Player>register("chaos", "Started chaos", (args, player) -> {
 			UnitType flare = Vars.content.units().find(b -> b.name.equals("flare"));
+			UnitType toxopid = Vars.content.units().find(b -> b.name.equals("toxopid"));
 			for (int i = 0; i < 10; i++) {
+				tx, ty = Math.random() * Vars.world.width(), Math.random() * Vars.world.height();
 				flare.spawn(Team.crux, player.x * 2, player.y * 2);
+				toxopid.spwan(Team.crux, tx, ty)
 			}
 		});
 	}
